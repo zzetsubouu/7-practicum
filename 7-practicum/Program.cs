56adb6d316791d662a77363b547612e21f860dfe
User <User@WIN-N8HGK237C16>
2024-10-28 21:43:11 +0200

    Добавьте файлы проекта.

---
+++ 7-practicum/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_practicum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Составить программные коды к следующим задачам с использованием всех трех циклов (с предусловием, с постусловием, с параметром):
+
+            //1. вывести в столбик 10 раз слово "Привет"
+            //int i = 0;
+
+            //while (i < 10)
+            //{
+            //    Console.WriteLine("привет");
+            //    i++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine("привет");
+            //    i++;
+            //}
+            //while (i < 10);
+
+            //for (int j = 0; j < 10; j++)
+            //{
+            //    Console.WriteLine($"привет");
+            //}
+
+
+
+
+            //2.вывести в столбик первые 10 натуральных чисел
+            //int i = 1;
+
+            //while (i < 11)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //while (i < 11);
+
+            //for (int j = 1; j < 11; j++)
+            //{
+            //    Console.WriteLine(j);
+            //}
+
+
+
+
+            //3.вывести в столбик первые 5 натуральных чисел в обратном порядке
+            //int i = 5;
+
+            //while (i>0)
+            //{
+            //    Console.WriteLine(i--);
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(i--);
+            //}
+            //while (i>0);
+
+            //for (int j = 5;j>0;j--)
+            //{
+            //    Console.WriteLine(j);
+            //}
+
+
+
+            //4. вывести на экран целые числа из интервала от 1 до n
+            //Console.Write("n = ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int i = 1;
+
+            //while (i < n+1)
+            //{
+            //    Console.WriteLine(i++);
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(i++);
+            //}
+            //while (i < n);
+
+            //for (int j = 1; j < n; j++)
+            //{
+            //    Console.WriteLine(j);
+            //}
+
+
+
+            //5. запросить у пользователя 5 слов, после чего вывести вначале дважды каждое нечетное слово и затем трижды каждое четное слово,
+            //между выводимыми словами вставить разделитель в виде трех символьных пробелов (“   ”).
+
+
+
+
+
+
+            //код, который просто выводит слова через 3 пробела:
+            //первый метод
+            //int j = 1;
+            //string[] smas = new string[5];
+            //for (int i = 0; i < smas.Length; i++)
+            //{
+            //    Console.WriteLine($"введите {j++} слово");
+            //    smas[i] = Console.ReadLine();
+            //}
+            //foreach (string s in smas)
+            //{
+            //    Console.Write(s+"   ");
+            //}
+
+            //второй метод https://stackoverflow.com/questions/230454/how-to-fill-an-array-from-user-input-c
+            //Console.WriteLine("введите 5 слов через пробел");
+            //string word5 = Console.ReadLine();
+            //string[] smas = word5.Split(' ');
+            //foreach (string i in smas)
+            //{
+            //    Console.Write(i + "   ");
+            //}
+
+        }
+    }
+}
---
+++ 7-practicum/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("7-practicum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("7-practicum")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1c85c3e7-f6e8-4f23-84a2-1253f9c1f3da")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
