10e686ecc8cbb8c40963715580f8c4653ad87848
zzetsubouu <154281553+zzetsubouu@users.noreply.github.com>
2024-11-04 00:27:41 +0200

    Update Program.cs

--- 7-practicum/Program.cs
+++ 7-practicum/Program.cs
@@ -106,33 +106,39 @@ namespace _7_practicum
 
             //5. запросить у пользователя 5 слов, после чего вывести вначале дважды каждое нечетное слово и затем трижды каждое четное слово,
             //между выводимыми словами вставить разделитель в виде трех символьных пробелов (“   ”).
-
-
-
-
-
-
-            //код, который просто выводит слова через 3 пробела:
-            //первый метод
-            //int j = 1;
-            //string[] smas = new string[5];
-            //for (int i = 0; i < smas.Length; i++)
+            
+            int j = 1;
+            string[] smas = new string[5];
+            for (int i = 0; i < smas.Length; i++)
+            {
+                Console.WriteLine($"введите {j++} слово");
+                smas[i] = Console.ReadLine();
+            }
+            int index = 0;
+            foreach (string element in smas)
+            {
+                index++;
+                if (index % 2 == 0)
+                {
+                    Console.WriteLine(element + "   " + element + "   " + element);
+                }
+                else if (index % 2 != 0)
+                {
+                    Console.WriteLine(element + "   " + element);
+                }
+            }
+            
+            //считает по количеству букв
+            //foreach (string element in smas)
             //{
-            //    Console.WriteLine($"введите {j++} слово");
-            //    smas[i] = Console.ReadLine();
-            //}
-            //foreach (string s in smas)
-            //{
-            //    Console.Write(s+"   ");
-            //}
-
-            //второй метод https://stackoverflow.com/questions/230454/how-to-fill-an-array-from-user-input-c
-            //Console.WriteLine("введите 5 слов через пробел");
-            //string word5 = Console.ReadLine();
-            //string[] smas = word5.Split(' ');
-            //foreach (string i in smas)
-            //{
-            //    Console.Write(i + "   ");
+            //    if (element.Length % 2 == 0)
+            //    {
+            //        Console.WriteLine(element + "   " + element + "   " + element);
+            //    }
+            //    else if (element.Length % 2 != 0)
+            //    {
+            //        Console.WriteLine(element + "   " + element);
+            //    }
             //}
 
         }
